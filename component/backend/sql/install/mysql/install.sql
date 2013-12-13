380af0860e563f62c5ea5399d069593c4bc82795
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2013-12-13 11:50:04 +0200

    Added auto-reply feature & support for multiple emails in category, separated by commas

--- component/backend/sql/install/mysql/install.sql
+++ component/backend/sql/install/mysql/install.sql
@@ -2,6 +2,8 @@ CREATE TABLE IF NOT EXISTS `#__contactus_categories` (
   `contactus_category_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(255) NOT NULL DEFAULT '',
   `email` varchar(255) DEFAULT '',
+  `sendautoreply` tinyint(3) DEFAULT 0,
+  `autoreply` text,
   `access` int(5) NOT NULL DEFAULT '1',
   `language` varchar(50) NOT NULL DEFAULT '*',
   `ordering` int(10) NOT NULL DEFAULT '0',
--- component/frontend/models/items.php
+++ component/frontend/models/items.php
@@ -19,31 +19,107 @@ class ContactusModelItems extends FOFModel
 
 		if ($result !== false)
 		{
-			// Get a reference to the Joomla! mailer object
-			$mailer = JFactory::getMailer();
-
-			// Set up the sender
-			$mailer->SetFrom($table->fromemail, $table->fromname);
-
-			// Load the category and set the recepient to this category's
-			// email address
-			$category = FOFModel::getTmpInstance('Categories', 'ContactusModel')
-				->getItem($table->contactus_category_id);
-			$mailer->addRecipient($category->email);
-
-			// Set the subject
-			$subject = JText::sprintf('COM_CONTACTUS_ITEMS_MSG_EMAIL_SUBJECT',
-				JFactory::getConfig()->get('sitename'),
-				$category->title,
-				$table->subject);
-
-			// Set the body
-			$mailer->MsgHTML($table->body);
-
-			// Send the email
-			$mailer->Send();
+			$this->_sendEmailToAdministrators($table);
+			$this->_sendEmailToUser($table);
 		}
 
 		return $result;
+	}
+
+	private function _sendEmailToAdministrators($table)
+	{
+		// Get a reference to the Joomla! mailer object
+		$mailer = JFactory::getMailer();
+
+		// Set up the sender
+		$mailer->SetFrom($table->fromemail, $table->fromname);
+
+		// Load the category and set the recipient to this category's
+		// email address
+		$category = FOFModel::getTmpInstance('Categories', 'ContactusModel')
+			->getItem($table->contactus_category_id);
+		$emails = explode(',', $category->email);
+
+		if (empty($emails))
+		{
+			return false;
+		}
+
+		foreach ($emails as $email)
+		{
+			$mailer->addRecipient($email);
+		}
+
+		// Set the subject
+		$subject = JText::sprintf('COM_CONTACTUS_ITEMS_MSG_EMAIL_SUBJECT',
+			JFactory::getConfig()->get('sitename'),
+			$category->title,
+			$table->subject);
+
+		$mailer->setSubject($subject);
+
+		// Set the body
+		$mailer->MsgHTML($table->body);
+
+		// Send the email
+		$mailer->Send();
+	}
+
+	private function _sendEmailToUser($table)
+	{
+		// Load the category and check the autoreply status
+		$category = FOFModel::getTmpInstance('Categories', 'ContactusModel')
+			->getItem($table->contactus_category_id);
+
+		if (!$category->sendautoreply)
+		{
+			return false;
+		}
+
+		$autoReply = $category->autoreply;
+		$autoReply = $this->_preProcessAutoreply($autoReply, $table, $category);
+
+		// Get a reference to the Joomla! mailer object
+		$mailer = JFactory::getMailer();
+
+		// Set up the sender
+		$fromemail = JFactory::getConfig()->get('mailfrom');
+		$fromname = JFactory::getConfig()->get('fromname');
+		$mailer->SetFrom($fromemail, $fromname);
+
+		$mailer->addRecipient($table->fromemail);
+
+		// Set the subject
+		$subject = JText::sprintf('COM_CONTACTUS_ITEMS_MSG_AUTOREPLY_SUBJECT', JFactory::getConfig()->get('sitename'));
+
+		$mailer->setSubject($subject);
+
+		// Set the body
+		$mailer->MsgHTML($autoReply);
+
+		// Send the email
+		$mailer->Send();
+	}
+
+	private function _preProcessAutoreply($text, FOFTable $item, FOFTable $category)
+	{
+		$replacements = array(
+			'[SITENAME]'		=> JFactory::getConfig()->get('sitename'),
+			'[CATEGORY]'		=> $category->title,
+		);
+
+		$rawData = $item->getData();
+
+		foreach ($rawData as $key => $value)
+		{
+			$replacements['[' . strtoupper($key) . ']'] = $value;
+		}
+
+		foreach ($replacements as $find => $changeTo)
+		{
+			$text = str_replace($find, $changeTo, $text);
+		}
+
+		return $text;
 	}
 }
