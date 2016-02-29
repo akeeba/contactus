e848b3f9db98c6e93a9a6762188bd8f7b81394e2
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2016-02-29 17:16:43 +0200

    Convert to FOF 3
    Make the front-end work

---
+++ component/frontend/Controller/ThankYou.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package		contactus
+ * @copyright	Copyright (c)2013-2016 Nicholas K. Dionysopoulos / AkeebaBackup.com
+ * @license		GNU General Public License version 2 or later
+ */
+
+namespace Akeeba\ContactUs\Site\Controller;
+
+defined('_JEXEC') or die();
+
+use FOF30\Controller\Controller;
+
+class ThankYou extends Controller
+{
+
+}
---
+++ component/frontend/Model/ThankYou.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package		contactus
+ * @copyright	Copyright (c)2013-2016 Nicholas K. Dionysopoulos / AkeebaBackup.com
+ * @license		GNU General Public License version 2 or later
+ */
+
+namespace Akeeba\ContactUs\Site\Model;
+
+defined('_JEXEC') or die();
+
+use FOF30\Model\Model;
+
+class ThankYou extends Model
+{
+
+}
