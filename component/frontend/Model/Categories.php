b46e488e58f5046e0e3e9f9cec998c74d02a1d16
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2016-05-16 16:48:16 +0300

    SimpleFactory in the frontend

---
+++ component/frontend/Model/Categories.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package		contactus
+ * @copyright	Copyright (c)2013-2016 Nicholas K. Dionysopoulos / AkeebaBackup.com
+ * @license		GNU General Public License version 2 or later
+ */
+
+namespace Akeeba\ContactUs\Site\Model;
+
+use FOF30\Model\DataModel;
+
+class Categories extends DataModel
+{
+}
---
+++ component/frontend/View/ThankYou/Html.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package		contactus
+ * @copyright	Copyright (c)2013-2016 Nicholas K. Dionysopoulos / AkeebaBackup.com
+ * @license		GNU General Public License version 2 or later
+ */
+
+namespace Akeeba\ContactUs\Site\View\ThankYou;
+
+use FOF30\View\DataView\Html as BaseView;
+
+class Html extends BaseView
+{
+}
