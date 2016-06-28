3fd34b90016a5cc815c34d0d2b9153b3d3f1985a
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2016-06-28 13:04:43 +0300

    Using the common build Phing script

---
+++ build/templates/link.php
@@ -0,0 +1,19 @@
+<?php
+$hardlink_files = array(
+);
+
+$symlink_files = array(
+);
+
+$symlink_folders = array(
+	// Build files
+	'../buildfiles/bin'							=> 'build/bin',
+	'../buildfiles/buildlang'					=> 'build/buildlang',
+	'../buildfiles/phingext'					=> 'build/phingext',
+	'../buildfiles/tools'						=> 'build/tools',
+	'../buildfiles/epub'						=> 'build/epub',
+
+	// Component translation
+	'translations/component/backend/en-GB'		=> 'component/language/backend/en-GB',
+	'translations/component/frontend/en-GB'		=> 'component/language/frontend/en-GB',
+);
