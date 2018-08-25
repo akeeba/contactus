39159a5c06114b4cbb48a0cd84336599815d4674
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2018-08-25 13:04:24 +0300

    Add GPG encryption to outgoing emails
    Make my experiment a Joomla! CLI script.

--- experiment/experiment.php
+++ experiment/experiment.php
@@ -7,14 +7,42 @@
 
 use Akeeba\ContactUs\Admin\Helper\PGP;
 
-define('_JEXEC', 1);
+if ($argc != 2)
+{
+	echo <<< END
+You must give the path to the root of a Joomla! site as the first and only argument of this script.
 
-/** @var \Composer\Autoload\ClassLoader $autoloader */
-$autoloader = (require_once '../component/backend/vendor/autoload.php');
-$autoloader->addPsr4('Akeeba\\ContactUs\\Admin\\Helper\\', __DIR__ . '/Helper');
+The Joomla! installation will be used to send the email. Make sure email sending is configured correctly on the Joomla!
+installation before proceeding.
 
-$publicKeyASCII = file_get_contents('encryption_key.asc');
-$secretKeyASCII = file_get_contents('signing_key.asc');
-$message        = file_get_contents('email.txt');
+END;
 
-echo PGP::signAndEncrypt($message, $publicKeyASCII, $secretKeyASCII);
+}
+
+$joomlaPath = $argv[1];
+$minphp     = '5.6.0';
+$curdir     = $joomlaPath . '/cli';
+require_once $joomlaPath . '/libraries/fof30/Cli/Application.php';
+
+class ExperimentWithPGPEncryption extends FOFCliApplication
+{
+	public function execute()
+	{
+		$this->importAutoloader();
+
+		$publicKeyASCII = file_get_contents('encryption_key.asc');
+		$secretKeyASCII = file_get_contents('signing_key.asc');
+		$message        = file_get_contents('email.txt');
+
+		echo PGP::signAndEncrypt($message, $publicKeyASCII, $secretKeyASCII);
+	}
+
+	private function importAutoloader()
+	{
+		/** @var \Composer\Autoload\ClassLoader $autoloader */
+		$autoloader = (require_once '../component/backend/vendor/autoload.php');
+		$autoloader->addPsr4('Akeeba\\ContactUs\\Admin\\Helper\\', __DIR__ . '/Helper');
+	}
+}
+
+FOFCliApplication::getInstance('ExperimentWithPGPEncryption')->execute();
