a91cb86f8964721d1bdfbb76ece1ff8dbf1bbde1
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2020-01-02 09:02:30 +0200

    Update copyright headers. Happy New Year!

--- build/templates/link.php
+++ build/templates/link.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * @package    contactus
+ * @copyright  Copyright (c)2013-2020 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @license    GNU General Public License version 3 or later
+ */
+
 $hardlink_files = [
 ];
 
