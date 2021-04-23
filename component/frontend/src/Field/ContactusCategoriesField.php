515ff00b68c80865c97a570822eec79b251b9933
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2021-04-23 07:56:32 +0300

    Converting to Core MVC
    Frontend, filter displayed categories

---
+++ component/frontend/src/Field/ContactusCategoriesField.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * @package    contactus
+ * @copyright  Copyright (c)2013-2021 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @license    GNU General Public License version 3 or later
+ */
+
+namespace Akeeba\Component\ContactUs\Site\Field;
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\Form\Field\ListField;
+use Joomla\CMS\Language\Multilanguage;
+use Joomla\Database\DatabaseDriver;
+use Joomla\Database\ParameterType;
+
+class ContactusCategoriesField extends ListField
+{
+	protected $type = 'ContactusCategories';
+
+	protected function getInput()
+	{
+		/** @var DatabaseDriver $db */
+		$db    = Factory::getContainer()->get('DatabaseDriver');
+		$query = $db->getQuery(true)
+			->select([
+				$db->qn('contactus_category_id'),
+				$db->qn('title'),
+			])->from($db->qn('#__contactus_categories'));
+
+		// Access filtering
+		$user       = Factory::getApplication()->getIdentity() ?: Factory::getUser();
+		$viewLevels = $user->getAuthorisedViewLevels();
+		$query->whereIn($db->quoteName('access'), $viewLevels);
+
+		// Multiple language filter
+		if (Multilanguage::isEnabled())
+		{
+			$languages = ['', '*', Factory::getApplication()->getLanguage()->getTag()];
+			$query->whereIn($db->quoteName('language'), $languages, ParameterType::STRING);
+		}
+
+		// Get the categories
+		$db->setQuery($query);
+		$objectList = $db->loadObjectList() ?? [];
+
+		foreach ($objectList as $o)
+		{
+			$this->addOption($o->title, [
+				'value' => $o->contactus_category_id,
+			]);
+		}
+
+		return parent::getInput();
+	}
+}
