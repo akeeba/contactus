6ce7e990d1678069f60b09dfa423a8bb4e5ccf66
Nicholas K. Dionysopoulos <nicholas@akeeba.com>
2023-07-20 11:22:07 +0300

    ~ Joomla 5 preparation: Use DatabaseInterface instead of DatabaseDriver

--- component/script.contactus.php
+++ component/script.contactus.php
@@ -15,4 +15,31 @@ class Pkg_ContactusInstallerScript extends InstallerScript
 	protected $minimumJoomla = '4.2.0';
 
 	protected $minimumPhp = '7.4.0';
+
+	function postflight($type, $parent)
+	{
+		// Do not run on uninstall.
+		if ($type === 'uninstall')
+		{
+			return true;
+		}
+
+		// Forcibly create the autoload_psr4.php file afresh.
+		if (class_exists(JNamespacePsr4Map::class))
+		{
+			try
+			{
+				$nsMap = new JNamespacePsr4Map();
+				$nsMap->create();
+				$nsMap->load();
+			}
+			catch (\Throwable $e)
+			{
+				// In case of failure, just try to delete the old autoload_psr4.php file
+				@unlink(JPATH_CACHE . '/autoload_psr4.php');
+			}
+		}
+
+		return true;
+	}
 }
